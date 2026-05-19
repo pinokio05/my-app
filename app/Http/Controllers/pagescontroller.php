9fb1f0ba306454d3e73456ea613ff643328b4e7b
Pinokio05 <carlosodhiambo22@gmail.com>
2026-05-19 13:40:10 +0300

    fixing a bug

--- app/Http/Controllers/pagescontroller.php
+++ app/Http/Controllers/pagescontroller.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class pagescontroller extends Controller
 {
     public function index(){
-        return view('pages.index')
+        return view('pages.index');
     }
 }
